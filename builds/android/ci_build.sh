8790100c8fa6c6979d106fbbb0603ecfb8c2dc94
Stephan Guilloux (cos) <stephan.guilloux@crisalid.com>
2022-09-30 17:04:18 +0200

    Problem: Out of sync with zproject
    Solution: Re-generate!

--- builds/android/ci_build.sh
+++ builds/android/ci_build.sh
@@ -7,7 +7,7 @@
 #   Exit if any step fails
 set -e
 
-export NDK_VERSION=android-ndk-r24
+export NDK_VERSION=android-ndk-r25
 export ANDROID_NDK_ROOT="/tmp/${NDK_VERSION}"
 
 case $(uname | tr '[:upper:]' '[:lower:]') in
